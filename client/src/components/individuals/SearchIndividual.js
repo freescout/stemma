a961fa9765a2c947d3dda7437a63bdb1fd98ebd6
freescout <neethu.jacob@gmail.com>
2021-01-06 09:22:21 +0100

    implemetes search individual

--- client/src/components/individuals/SearchIndividual.js
+++ client/src/components/individuals/SearchIndividual.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 import IndividualDataService from '../../services/IndividualService';
+
+
 
 const SearchIndividual = (props) => {
 
@@ -13,13 +17,32 @@ const SearchIndividual = (props) => {
   const [searchDetails, setSearchDetails] = useState(initialSearchDetails);
   const [individuals, setIndividuals] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(-1);
+  const [show, setShow] = useState(false);
+  const [selectedIndividual, setSelectedIndividual] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   const handleInputChange = event => {
     const { name, value } = event.target;
     setSearchDetails({ ...searchDetails, [name]: value }
     )
   };
+
+  const setActiveIndividual = (individual, index) => {
+    console.log("setActive Indiv", individual);
+    setCurrentIndex(currentIndex);
+    setSelectedIndividual(individual);
+  }
+
+  const selectIndividual = () => {
+    props.getIndividual(selectedIndividual);
+    setShow(false);
+  }
+
   const searchName = () => {
+    setShow(true);
     console.log("Searchname calling", searchDetails.firstName + searchDetails.lastName);
     IndividualDataService.findByName(searchDetails.firstName, searchDetails.lastName)
       .then(response => {
@@ -36,7 +59,7 @@ const SearchIndividual = (props) => {
   }
   return (
     <div>
-      <div>{props.name}</div>
+      
       <div class="form-row mb-4">
         <div class="col">
           <input type="text" id="firstName" class="form-control" required placeholder="First name" value={searchDetails.firstName} onChange={handleInputChange} name='firstName' />
@@ -59,26 +82,40 @@ const SearchIndividual = (props) => {
       >
         Search
       </button>
-</div>
-      <h4>Select Partner from the  List</h4>
-      <div>{JSON.stringify(individuals)}</div>
-      <div>{console.log(JSON.stringify(individuals))}</div>
-       <ul className="list-group">
-        {individuals &&
-          individuals.map((individual, index) => (
+    </div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Select Partner from the  List</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {individuals &&
+            individuals.map((individual, index) => (
 
-            <li
-              className={"list-group-item" + (index === currentIndex ? "active" : "")}
-              onClick={() => this.setActiveindividual(individual, index)}
-              key={index}
-            >
-              {individual.name.firstName} {" "} {individual.name.lastName}
-            </li>
-          ))}
-      </ul>  
+              <li
+                className={"list-group-item" + (index === currentIndex ? "active" : "")}
+                onClick={() => setActiveIndividual(individual, index)}
+                key={index}
+              >
+                {individual.name.firstName} {" "} {individual.name.lastName}
+              </li>
+            ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={selectIndividual}>Select</Button>
+        </Modal.Footer>
+      </Modal>
+
     </div>
   )
-  console.log("Searchname calling", props.name);
+  
 
 }
 
